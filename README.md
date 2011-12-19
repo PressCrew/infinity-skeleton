14e601c234be33525a87386c53505701f01741b4
Marshall Sorenson <marshall.sorenson@gmail.com>
2011-12-19 14:29:15 -0500

    original source

---
+++ README.md
@@ -0,0 +1,28 @@
+## Infinity Skeleton Child Theme
+
+Infinity Skeleton is a bare bones child theme for the Infinity Theming Engine.
+
+### Authors
+
+* Marshall Sorenson
+* Bowe Frankema
+
+### License
+
+Infinity Skeleton is licensed under GPL v2.0 or higher.
+See LICENSE.txt for full details.
+
+### Requirements
+
+* PHP 5.2.4 or higher - http://www.php.net
+* WordPress 3.2.0 or higher - http://wordpress.org/download
+* Infinity 1.0b2 or higher - https://github.com/MrMaz/infinity/tags
+
+### Support
+
+* http://infinity.presscrew.com/docs
+* http://infinity.presscrew.com/api
+
+### Community
+
+* http://community.presscrew.com
---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Set this to true to put Infinity into developer mode
+ */
+//define( 'INFINITY_DEV_MODE', true );
+
+//
+// Usually dev mode is enough, but if want finer control you can
+// set some of these special constants manually.
+//
+
+/**
+ * Set this to false to totally disable error handling by Infinity
+ */
+//define( 'INFINITY_ERROR_HANDLING', false );
+
+/**
+ * Set this to true to show detailed error and exception reports. This only
+ * works if error handling is enabled (see above)
+ */
+//define( 'INFINITY_ERROR_REPORTING', true );
+
+/**
+ * Set this to a short message to customize the friendly AJAX error
+ */
+//define( 'PIE_EASY_ERROR_AJAX_MESSAGE', 'Woops! We forgot to feed the server, sorry!' );
+
+/**
+ * Set this to an absolute path to a custom friendly fatal error page
+ */
+//define( 'PIE_EASY_ERROR_PAGE_PATH', '/path/to/my/error.php' );
+
+/**
+ * Set this to false to disable caching of dynamically generated
+ * CSS and Javascript. Highly recommended for development.
+ */
+//define( 'PIE_EASY_CACHE_EXPORTS', false );
+
+?>
